5a346b6025ea76e491ecb6486e0ac8684a264368
kucaahbe <kucaahbe@ukr.net>
2011-07-18 19:00:40 +0300

    added log function

--- rc.lua
+++ rc.lua
@@ -1,1 +1,8 @@
+-- utils
+function log(text)
+  print('[awesome-' .. awesome.version .. '] ' .. text)
+end
+
+log('============= starting =============')
 awesome.spawn('google-chrome')
+log('------------- started --------------')
