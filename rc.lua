90babc837367f25a807666b6305263ea8de949a4
kucaahbe <kucaahbe@ukr.net>
2011-07-18 19:05:31 +0300

    some variables

--- rc.lua
+++ rc.lua
@@ -1,8 +1,23 @@
 -- utils
-function log(text)
+local log = function(text)
   print('[awesome-' .. awesome.version .. '] ' .. text)
 end
 
 log('============= starting =============')
+
+-- awesome standard libs
+require('awful')
+
+-- variables
+local confdir   = awful.util.getdir('config')
+local conffile  = confdir .. '/rc.lua'
+local themefile = confdir .. '/themes/default/theme.lua'
+modkey = "Mod4"
+
+log("config directory: '" .. confdir   .. "'")
+log("theme file:       '" .. themefile .. "'")
+log("config file:      '" .. conffile .. "'")
+
 awesome.spawn('google-chrome')
+
 log('------------- started --------------')
