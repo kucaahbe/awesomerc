192c82ff0cf7a9d618d865f037260a5b8fe3d8c2
kucaahbe <kucaahbe@ukr.net>
2011-07-20 21:13:18 +0300

    added themename

--- rc.lua
+++ rc.lua
@@ -15,14 +15,16 @@ require("awful.remote")
 -- variables
 local confdir   = awful.util.getdir('config')
 local conffile  = confdir .. '/rc.lua'
-local themefile = confdir .. '/themes/default/theme.lua'
+local themename = 'default'
+local themefile = confdir .. '/themes/'.. themename .. '/theme.lua'
 
 terminal = 'lilyterm'
 modkey = "Mod4"
 
 log("config directory: '" .. confdir   .. "'")
-log("theme file:       '" .. themefile .. "'")
 log("config file:      '" .. conffile .. "'")
+log("theme file:       '" .. themefile .. "'")
+log("theme name:       '" .. themename .. "'")
 
 beautiful.init(themefile)
 
