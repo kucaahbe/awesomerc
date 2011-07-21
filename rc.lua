367b0068d43154fb31ba4824ff6bbfa3feea6cca
kucaahbe <kucaahbe@ukr.net>
2011-07-21 22:24:15 +0300

    removed fulscreen layout

--- rc.lua
+++ rc.lua
@@ -40,7 +40,7 @@ layouts =
     awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
+  --awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier
 }
 
